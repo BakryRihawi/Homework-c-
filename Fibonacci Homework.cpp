a54f00b1d186bce1518c5914afffb221e5cffbfe
DESKTOP-IQVHD5N\Bakry Rehawi <bakry29bbb@gmail.com>
2022-11-07 13:33:05 +0200

    Homework

---
+++ Fibonacci Homework.cpp
@@ -0,0 +1,32 @@
+﻿// Fibonacci Homework.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+unsigned int Fibonacci(unsigned int Number)
+{
+
+	if (Number == 0 || Number == 1)
+		return Number;
+	else
+	{
+		unsigned int Fibonaccii = Fibonacci(Number - 1) + Fibonacci(Number - 2);
+		return Fibonaccii;
+	}
+
+}
+int main()
+{
+	unsigned int NumberFibonacci = 0;
+	cout << "Please Enter The NumberFibonacci: \t";
+	cin >> NumberFibonacci;
+	for (int i = 0; i < NumberFibonacci; i++)
+	{
+		cout << Fibonacci(i) << "\t";
+
+	}
+
+
+}
+
---
+++ Numbers ascending or descending order.cpp
@@ -0,0 +1,60 @@
+﻿// Numbers ascending or descending order.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+	int ArraysNumber[400], i, j, ArraySize, tempo;
+	char Sort_method;
+
+	
+	void OrderOfNumbers(int ArraySize,char Sort_method)
+	{
+		
+		cout << "Enter the Array Number\n";
+		for (int i = 0; i < ArraySize; i++)
+		{
+			cout << "ArraysNumber(" << i << ") = ";
+			cin >> ArraysNumber[i];
+		}
+		for (int i = 0; i < ArraySize; i++) 
+
+			for (int j = i + 1; j < ArraySize; j++){
+				if (Sort_method == 'A') {
+					if (ArraysNumber[i] > ArraysNumber[j]) {
+						tempo = ArraysNumber[i];
+						ArraysNumber[i] = ArraysNumber[j];
+						ArraysNumber[j] = tempo;
+					}
+				}
+				else if (Sort_method == 'D') {
+					if (ArraysNumber[i] < ArraysNumber[j]) {
+						tempo = ArraysNumber[i];
+						ArraysNumber[i] = ArraysNumber[j];
+						ArraysNumber[j] = tempo;
+					}
+				}
+
+		}
+
+
+
+		for (int i = 0; i < ArraySize; i++) {
+			cout << ArraysNumber[i] << " ";
+		}
+
+	}
+
+
+
+
+int main()
+{
+	
+	
+	OrderOfNumbers(5,'D');
+
+
+}
+
